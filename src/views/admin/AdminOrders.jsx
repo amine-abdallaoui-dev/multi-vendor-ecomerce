53273728a86355916f53d7350ba0af82a0f6e71b
amine-abdallaoui-dev <thekasper2030@gmail.com>
2026-02-27 07:11:10 +0100

    update

--- src/views/admin/AdminOrders.jsx
+++ src/views/admin/AdminOrders.jsx
@@ -7,7 +7,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 const AdminOrders = () => {
     return (
-        <div className="w-full md:ml-5 bg-white rounded-md md:overflow-hidden overflow-auto h-auto">
+        <div className="w-full  md:ml-5 bg-white rounded-md md:overflow-hidden overflow-auto h-auto">
             <div className="items-center flex md:px-[25px] w-full h-[80px]">
                 <form className=" hidden md:flex  justify-between w-full">
                     <select className=" md:px-3 py-2 bg-[#edf5f5] outline-none border-[#edf5f5] ">
@@ -18,60 +18,43 @@ const AdminOrders = () => {
                     <input className="md:w-[15%] md:px-3 md:py-2 bg-[#edf5f5] outline-none border-[#edf5f5]"  type="text" placeholder="Search ..."/>
                 </form>
             </div>
-            <div className="w-full bg-[#edf5f5] flex md:ml-5 px-2 mt-[30px] h-auto md:overflow-hidden overflow-auto mb-[20px]">
-                <div className="flex justify-start items-start flex-col w-full ">
-                    <div className=" w-full -700 border-2 py-2 border-b-[#ccd5ae] text-start">
-                        <div className="flex  justify-between py-2  items-center w-full px-2">
-                            <h6 className="flex justify-start font-[400] w-[160px]">Order Id</h6>
-                            <h6 className=" font-[400] w-[16%]">Price</h6>
-                            <h6 className=" flex justify-start font-[400] w-[16%]">Payment Status</h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]">Status</h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]">Action</h6>
-                            <h6 className="flex justify-start   font-[400] w-[16%]"><FaArrowDownShortWide /></h6>
-                        </div>
-                    </div>
-                    <div className=" w-full  flex justify-start py-2 border-2 border-b-[#ccd5ae]">
-                        <div className="flex justify-between  items-center w-full py-2 px-2   ">
-                            <h6 className="flex justify-start  font-[400] w-[13%]">#7Y7Y7H</h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]">$170</h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]">Pending</h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]">Pending</h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]"><Link to="">View All</Link></h6>
-                            <h6 className="flex justify-start  font-[400] w-[16%]"><FaArrowDownShortWide /></h6>
-                        </div>
-                    </div>
+            <div className="w-full mx-5 bg-[#edf5f5] p-5 mt-[30px] h-auto md:overflow-hidden overflow-auto mb-[20px]">
+                <table className="w-full ">
+                    <tr className="text-sm font-medium border-b border-gray-600">
+                        <td className="w-[20%] text-gray-900 py-4">Order Id</td>
+                        <td className="w-[20%] text-gray-900">Price</td>
+                        <td className="w-[20%] text-gray-900">Payment Status</td>
+                        <td className="w-[20%] text-gray-900">Order Status</td>
+                        <td className="w-[20%] text-gray-900">Action</td>
+                    </tr>
                     {
-                        [1,2,3,4,5].map((item,index) => {
+                        [1,2,3,4,5].map((item, index) => {
                             return (
-                                <div className=" w-full  flex justify-start py-2 border-2 border-b-[#ccd5ae]">
-                                    <div className="flex justify-between  items-center w-full py-2 px-2   ">
-                                        <h6 className="flex justify-start  font-[400] w-[13%]">#7Y7Y7H</h6>
-                                        <h6 className="flex justify-start  font-[400] w-[16%]">$170</h6>
-                                        <h6 className="flex justify-start  font-[400] w-[16%]">Pending</h6>
-                                        <h6 className="flex justify-start  font-[400] w-[16%]">Pending</h6>
-                                        <h6 className="flex justify-start  font-[400] w-[16%]"><Link to="">View All</Link></h6>
-                                        <h6 className="flex justify-start  font-[400] w-[16%]"><FaArrowDownShortWide /></h6>
-                                    </div>
-                                </div>
+                                <tr className="text-sm font-[400] w-full ">
+                                    <td className="w-[20%] text-gray-900 py-4">#757656</td>
+                                    <td className="w-[20%] text-gray-900">$140</td>
+                                    <td className="w-[20%] text-gray-900">Pending</td>
+                                    <td className="w-[20%] text-gray-900">Pending</td>
+                                    <td className="w-[20%] text-gray-900"><Link to="">View order</Link></td>
+                                </tr>
                             )
                         })
                     }
-                </div>
-
+                </table>
             </div>
-            <div className="w-full flex justify-start ml-6 gap-1 items-center mb-3">
-                <span className="py-3 px-3 rounded-md bg-[#069f96] text-white font-medium">
+            <div className="w-[95%] flex justify-end mr-10 gap-1 items-center mb-3">
+                <span className="py-3 px-3 rounded-md bg-[#069f96] text-white font-medium hover:bg-green-400">
                     <FaArrowLeftLong/>
                 </span>
-                <span className="py-1 px-3 rounded-md bg-[#069f96] text-white font-medium">1</span>
+                <span className="py-1 px-3 rounded-md bg-[#069f96] text-white font-medium hover:bg-green-400">1</span>
                 {
                     [1,2,3,4,5].map((item,index) => {
                         return (
-                            <span className="py-1 px-3 rounded-md bg-gray-400 text-white font-medium">{item}</span>
+                            <span className="py-1 px-3 rounded-md bg-gray-400 text-white font-medium hover:bg-green-400">{item}</span>
                         )
                     })
                 }
-                <span className="py-3 px-3 rounded-md bg-[#069f96] text-white font-medium">
+                <span className="py-3 px-3 rounded-md bg-[#069f96] text-white font-medium hover:bg-green-400">
                     <FaArrowRightLong/>
                 </span>
 
